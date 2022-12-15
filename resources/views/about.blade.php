8f7c0d90bb5eff1a914fca02e410a3e38bcf07ef
Salano24 <salernomarco48@email.com>
2022-12-15 16:28:53 +0100

    fine esercizio +bonus

---
+++ resources/views/about.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+</head>
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body{
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        background-color: #007acc;
+    }
+
+    .navigation{
+        display: flex;
+        width: 80%;
+        margin: auto;
+        justify-content: space-between;
+        align-items: center ;
+        padding: 1rem 1rem;
+        
+    }
+
+    a{
+        color: white;
+        text-decoration: none;
+        margin-left: 2rem;
+    }
+
+    p{
+        color: white;
+        font-weight: 800;
+    }
+
+    h1{
+        color: white;
+    }
+
+    .text{
+        width: 70%;
+        margin: auto;
+        margin-top: 2rem;
+    }
+</style>
+<body>
+    <div class="navigation">
+       <h1>ABOUT</h1>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+    </nav> 
+    </div>
+    <div class="text">
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta ad laborum quod facere eaque autem labore natus officia, ipsa nam provident voluptate consequuntur inventore aut nobis maiores eum accusantium harum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis nam corporis dolorum non ipsum nemo fugit temporibus, possimus, pariatur nihil sint quis quos numquam similique quam dolores error est. Ex. Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, magnam quaerat repudiandae reprehenderit et nulla facere in hic fuga corrupti. Necessitatibus odio voluptatum natus modi, eum placeat asperiores corrupti facere?</p>
+    </div>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel</title>
+    <style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    body{
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        background-color: #007acc;
+    }
+    .nav{
+        width: 80%;
+        margin: auto;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem 1rem;
+
+    }
+    
+    a{
+        color: white;
+        text-decoration: none;
+        margin-left: 2rem;
+    }
+
+    h1{
+        color: white;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="nav">
+        <h1>HELLO LARAVEL!</h1>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+    </nav> 
+    </div>
+   
+</body>
+</html>
